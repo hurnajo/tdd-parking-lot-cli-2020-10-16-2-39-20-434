d3ab5f622e268fb95fe685678733c337c8c46094
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-16 21:30:30 +0800

    [TDD - Parking Lot][Leo] Feature - Implement Logic for parking lot with capacity

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,18 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
+    private final int capacity;
     private final Map<ParkingTicket,Car> ticketCarMap = new HashMap<>();
+
+    public ParkingLot() {
+        this(10);
+    }
+
+    public ParkingLot(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getAvailableParkingPosition() {
+        return ticketCarMap.size() - capacity;
+    }
+
     public ParkingTicket park(Car car) {
         ParkingTicket ticket = new ParkingTicket();
         ticketCarMap.put(ticket,car);
-        return ticket;
+        return getAvailableParkingPosition()<0?ticket:null;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
         return ticketCarMap.get(parkingTicket);
     }
 
-    public boolean isValidTicket(ParkingTicket parkingTicket){
-        return parkingTicket == null ? true : false;
+    public boolean isParkingLotFull(Car car){
+        return park(car) == null ? true : false;
     }
+
 }
