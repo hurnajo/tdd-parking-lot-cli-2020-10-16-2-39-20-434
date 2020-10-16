cb1741e050808a70b9391da859ae72082a657f73
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-16 15:30:12 +0800

    [TDD - Parking Lot][Leo] Feature - Implement Logic for no car fetched when fetch car with wrong ticket

--- src/main/java/com/oocl/cultivation/ParkingTicket.java
+++ src/main/java/com/oocl/cultivation/ParkingTicket.java
@@ -1,5 +1,11 @@
 package com.oocl.cultivation;
 
 public class ParkingTicket extends ParkingLot {
+    public ParkingTicket(ParkingLot parkingLot){
 
+    }
+
+    public ParkingTicket() {
+
+    }
 }
