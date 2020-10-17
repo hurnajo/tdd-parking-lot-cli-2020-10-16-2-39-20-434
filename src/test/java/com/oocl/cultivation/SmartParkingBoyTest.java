e6a2b06b1f569627dee74ef79bbc38199f44ff92
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-17 17:44:50 +0800

    [TDD - Parking Lot Story 4][Leo] Test- Create Test case for returning correct ticket

--- src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -7,21 +7,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class SmartParkingBoyTest {
 
     @Test
-    public void should_return_a_parking_ticket_when_park_given_a_car() {
-        // GIVEN
+    public void should_return_parking_ticket_when_park_given_car() {
+        //given
         Car car = new Car();
         List<ParkingLot> parkingLotList = new ArrayList<>();
         parkingLotList.add(new ParkingLot());
         ParkingBoy parkingBoy = new SmartParkingBoy(parkingLotList);
 
-        // WHEN
+        //when
         ParkingTicket parkingTicket = parkingBoy.park(car);
 
-        // THEN
+        //then
         assertNotNull(parkingTicket);
     }
+
+    @Test
+    public void should_return_correct_car_when_fetch_given_correct_ticket() {
+        // GIVEN
+        Car car = new Car();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(new ParkingLot());
+        ParkingBoy parkingBoy = new SmartParkingBoy(parkingLotList);
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+
+        // WHEN
+        Car fetchedCar = parkingBoy.fetch(parkingTicket);
+
+        // THEN
+        assertSame(fetchedCar, car);
+    }
+
+
 }
