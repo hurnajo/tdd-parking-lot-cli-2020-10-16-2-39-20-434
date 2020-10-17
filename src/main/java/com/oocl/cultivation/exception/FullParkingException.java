85be55e732f6a2ff8dc5471e391e8857686d276d
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-17 16:23:17 +0800

    [TDD - Parking Lot][Leo] Feature - Create Exception package and handling

---
+++ src/main/java/com/oocl/cultivation/exception/FullParkingException.java
@@ -0,0 +1,7 @@
+package com.oocl.cultivation.exception;
+
+public class FullParkingException extends RuntimeException {
+    public FullParkingException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/oocl/cultivation/exception/UnrecognizedParkingTicketException.java
+++ src/main/java/com/oocl/cultivation/exception/UnrecognizedParkingTicketException.java
@@ -1,6 +1,6 @@
 package com.oocl.cultivation.exception;
 
-public class UnrecognizedParkingTicketException extends Exception {
+public class UnrecognizedParkingTicketException extends RuntimeException {
     public UnrecognizedParkingTicketException(String message){
         super(message);
     }
