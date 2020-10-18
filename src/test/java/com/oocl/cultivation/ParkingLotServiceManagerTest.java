4241154877c521402a225e90627f289e0e8df0cb
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-18 09:54:37 +0800

    [TDD - Parking Lot][Leo] Test- Create Test case for parking lot service manager management list

---
+++ src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
@@ -0,0 +1,36 @@
+package com.oocl.cultivation;
+
+import com.oocl.cultivation.smartParkingBoy.SmartParkingBoy;
+import com.oocl.cultivation.smartParkingBoy.SuperSmartParkingBoy;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotServiceManagerTest {
+    Car car = new Car();
+    private List<ParkingLot> parkingLot1 = new ArrayList<>();
+    private List<ParkingLot> parkingLot2 = new ArrayList<>();
+    private List<ParkingLot> parkingLot3 = new ArrayList<>();
+    ParkingBoy parkingBoy = new ParkingBoy(parkingLot1);
+    ParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot2);
+    ParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLot3);
+    ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager();
+    @Test
+    void should_return_management_List_when_adding_parking_boys_given_management_List() {
+        //given
+        parkingLot1.add(new ParkingLot());
+        parkingLot2.add(new ParkingLot());
+        parkingLot3.add(new ParkingLot());
+        List<ParkingBoy> parkingBoyList = Arrays.asList(parkingBoy,smartParkingBoy,superSmartParkingBoy);
+        parkingManager.setManageList(parkingBoys);
+        //when
+        List<ParkingBoy> getManageList = parkingManager.getManageList();
+        //then
+        assertNotNull(getManageList);
+    }
+}
