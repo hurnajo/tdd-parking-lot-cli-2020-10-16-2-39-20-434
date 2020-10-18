f20cb1df3d6cad264c374209be44a732dffca836
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-18 11:56:29 +0800

    [TDD - Parking Lot Story 6][Leo] Feature - Implement Logic to commandToFetch

--- src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
@@ -6,25 +6,24 @@ import java.util.List;
 public class ParkingLotServiceManager extends ParkingBoy {
     private List<ParkingBoy> managementList = new ArrayList<>();
 
-    public ParkingLotServiceManager(List<ParkingLot> parkingLotList){
+    public ParkingLotServiceManager(List<ParkingLot> parkingLotList) {
         super(parkingLotList);
     }
-
-
-    public void setManagementList(List<ParkingBoy> parkingBoys){
-        this.managementList=parkingBoys;
-    }
-
-    public List<ParkingBoy> getManagementList() {
-        return managementList;
-    }
-
 
     public ParkingTicket commandToPark(ParkingBoy parkingBoy, Car car) {
         return parkingBoy.park(car);
     }
 
     public Car commandToFetch(ParkingBoy parkingBoy, ParkingTicket parkingTicket) {
-        return null;
+        return parkingBoy.fetch(parkingTicket);
     }
+
+    public void setManagementList(List<ParkingBoy> parkingBoys) {
+        this.managementList = parkingBoys;
+    }
+
+    public List<ParkingBoy> getManagementList() {
+        return managementList;
+    }
+
 }
