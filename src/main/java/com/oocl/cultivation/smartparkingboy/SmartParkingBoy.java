9d758e01b19ff69ccd0edb4fb6c10ca20620f4bf
HURNAJO <jose.leo.hurna@oocl.com>
2020-10-19 20:15:05 +0800

    [TDD - Parking Lot][Leo] Refactor - rename getParkingLot to findParkingLot and make filter first before comparing

--- src/main/java/com/oocl/cultivation/smartparkingboy/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/smartparkingboy/SmartParkingBoy.java
@@ -1,10 +1,13 @@
-package com.oocl.cultivation.smartParkingBoy;
+package com.oocl.cultivation.smartparkingboy;
 
 import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.exception.FullParkingException;
 
 import java.util.Comparator;
 import java.util.List;
+
+import static com.oocl.cultivation.constant.Constant.NOT_ENOUGH_SPACE;
 
 public class SmartParkingBoy extends ParkingBoy {
     public SmartParkingBoy(List<ParkingLot> parkingLotList) {
@@ -12,10 +15,11 @@ public class SmartParkingBoy extends ParkingBoy {
     }
 
     @Override
-    public ParkingLot getParkinglot() {
-        return  parkingLotList.stream().max(Comparator.comparing(ParkingLot::getEmptySpace))
-                .filter(a -> a.getMapSize()!=a.getParkingLotCapacity())
-                .orElseThrow(() -> new RuntimeException(NOT_ENOUGH_SPACE));
+    public ParkingLot findParkingLot() {
+        return  parkingLotList.stream()
+                .filter(parkingLot -> parkingLot.getMapSize()!=parkingLot.getParkingLotCapacity())
+                .max(Comparator.comparing(ParkingLot::getEmptySpace))
+                .orElseThrow(() -> new FullParkingException(NOT_ENOUGH_SPACE));
     }
 
 }
